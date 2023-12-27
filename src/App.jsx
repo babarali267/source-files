063f7f2d65c9c21797c51a155e218e029daacd2e
Babar Ali <144090286+babarali267@users.noreply.github.com>
2023-12-27 14:58:48 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import TestimonialSlider from "./compoents/Testimonial/testimonial.component"
+import About from "./compoents/about/about.compoent"
+import Footer from "./compoents/footer/footer.compoent"
+import Header from "./compoents/header/header.compoent"
+import Portfolio from "./compoents/portfolio/portfolio.component"
+import Services from "./compoents/services/service.component"
+ 
+ 
+function App() {
+ 
+
+  return (
+    <>
+      <Header/>
+      <About />
+      <Services />
+      <Portfolio />
+      <TestimonialSlider />
+      <Footer/>
+    </>
+  )
+}
+
+export default App
